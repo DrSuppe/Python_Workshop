480bc020d48cabafe26b16f031c6331f2c05a228
TimKayser <timjustuskayser@web.de>
2020-04-21 22:13:06 +0200

    Initial Commit

---
+++ cell.py
@@ -0,0 +1,10 @@
+class myTinyCell:
+
+    def __init__(self, state, xPos, yPos):
+        self.state = state
+        self.xPos = xPos
+        self.yPos = yPos
+
+    def countNeighbours(self):
+
+
