10e6e0e0d4b0b92a3f9eb738d441aefdff111cd5
J.Kayser <j.kayser@tu-braunschweig.de>
2020-05-02 14:33:10 +0200

    ein wenig weiter gearbeitet

--- cell.py
+++ cell.py
@@ -1,23 +1,17 @@
 class myTinyCell:
 
-    def __init__(self, x, y):
+    def __init__(self):
         self.state = False
-        self.nextstate = False
-        self.y = y
-        self.x = x
+        self.nextState = False
+        #self.y = y
+        #self.x = x
         pass
-
-    def countNeighbours(self, spielFeld):
-
-        nNeighbours = 0
-
-        for i in range(-1, 2):
-           for j in range(-1, 2):
-               pass
-
 
     def test(self, x, y):
         print("Koordinaten:", x, ", ", y)
+
+    def setState(self, nextstate):
+        self.nextState = nextstate
 
 
 
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from cell import myTinyCell
-import numpy as np
 
 class gameOfLife():
     def __init__(self):
@@ -13,8 +12,30 @@ class gameOfLife():
         self.arr = [[myTinyCell() for i in range(nCol)] for j in range(nRows)]
         pass
 
+    def nextCycle(self):
+
+        for i in range(1, self.nRows -1):
+            for j in range(1, self.nCollumns):
+                if self.countNeighbours(i, j) <= 1 or self.countNeighbours(i, j) >=4:
+                    self.arr[i][j].nextState = False
+                else:
+                    self.arr[i][j].nextState = True
+
+    def countNeighbours(self, x, y):
+
+        nNeighbours = 0
+
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if self.arr[i][j].state == True:
+                    nNeighbours += 1
+
+        return nNeighbours
+
     def play(self):
         pass
+
+
 
 
 
@@ -25,3 +46,7 @@ test = gameOfLife()
 test.createField(2, 2)
 
 print(test.arr)
+
+for i in range(0, 2):
+    for j in range(0, 2):
+        test.arr[i][j].test(i, j)
