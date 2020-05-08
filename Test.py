db934a7ebc9eb79ba94c6e6d5698767ffd4f78ac
TimKayser <timjustuskayser@web.de>
2020-05-08 13:12:39 +0200

    recplaced all files. Logic is now the program we created in the course.
    This is also the fully commented version

--- Test.py
+++ Test.py
@@ -1,32 +1,3 @@
-#konsolenausgabe
-print("Hello World")
-
-#grundlegende Datentypen
-ganzeZahl = 1 #int
-kommaZahl = 1.3 #float
-Wort  = "Wort" # String
-Boolean = 3 # False
-#TODO Array
-
-
-# if und elif
-if Boolean <= 2:
-  print("Falsch")
-elif Boolean >=4:
-  print("richtig")
-else:
-  print("didney worl")
-
-
-#for Schleife
-for i in range(0, 101):
-  print(i)
-
-
-#while-Schleife
-zaehlervariable = 0
-while True:
-  zaehlervariable += 1
-  print("counter: ", zaehlervariable)
-  if zaehlervariable >= 100:
-    break
+for i in range(-1, 2):
+    for j in range(-1, 2):
+        print("Zeile: ", i,", Spalte: ", j)
--- population.py
+++
@@ -1,5 +0,0 @@
-class myCellPopulation:
-    height = 100
-    width = 100
-
-    def __init__(self):
